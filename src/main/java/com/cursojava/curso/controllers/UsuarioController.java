dcf9ec11a1b3028a59207d31a9a061926f2f0098
magno309 <luisabrahamsanabriamelecio@gmail.com>
2022-05-05 13:01:57 -0500

    Llamar API con Ajax

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.cursojava.curso.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.cursojava.curso.models.Usuario;
 
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +22,40 @@ public class UsuarioController {
         usuario.setTelefono("1323216585");
         usuario.setPassword("melecio");
         return usuario;
+    }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getListUsuario() {
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(1);
+        usuario.setNombre("Luis");
+        usuario.setApellido("Sanabria");
+        usuario.setEmail("luis@mail.com");
+        usuario.setTelefono("1323216585");
+        usuario.setPassword("melecio");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2);
+        usuario2.setNombre("Luis");
+        usuario2.setApellido("Sanabria");
+        usuario2.setEmail("luis@mail.com");
+        usuario2.setTelefono("1323216585");
+        usuario2.setPassword("melecio");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3);
+        usuario3.setNombre("Luis");
+        usuario3.setApellido("Sanabria");
+        usuario3.setEmail("luis@mail.com");
+        usuario3.setTelefono("1323216585");
+        usuario3.setPassword("melecio");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+
+        return usuarios;
     }
 
     @RequestMapping(value = "usuario1")
